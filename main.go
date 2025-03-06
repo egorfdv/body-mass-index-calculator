4660f0dbd5d93d9a8707b6cefa4bd699441b125c
egorfdv <egorfdvru@gmail.com>
2025-03-06 18:59:31 +0300

    Добавлено форматирование строк и считывание ввод пользольвателя

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	userHeight := 1.8
-	userWeight := 100.0
-	var IMT = userWeight / math.Pow(userHeight, 2)
-	fmt.Print(IMT)
+	const IMTPower = 2
+	var userHeight float64
+	var userWeight float64
+	fmt.Println("__ Калькулятор индекса массы тела __ ")
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userWeight)
+	IMT := userWeight / math.Pow(userHeight/100, IMTPower)
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
+	fmt.Print(result)
 }
 
 // var userHeight float64 = 1.8
@@ -19,3 +26,8 @@ func main() {
 // userKg = 100
 // var userHeight, userKg float64 = 1.8, 100
 // userHeight, userKg := 1.8, 100
+// fmt.Print("__ Калькулятор индекса массы тела __ \n")
+// fmt.Print("Ваш индекс", IMT)
+// fmt.Printf("Ваш индекс массы тела: %v", IMT)
+// fmt.Print(`__ Калькулятор индекса массы тела __
+// Введите свой рост в сантиметрах: %.1f`, 0,1)
