2d26e76c0877ba318ee194650256ff5fbcd3a490
egorfdv <egorfdvru@gmail.com>
2025-03-06 17:21:36 +0300

    Объявлены переменные

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello")
+	userHeight := 1.8
+	userWeight := 100.0
+	var IMT = userWeight / math.Pow(userHeight, 2)
+	fmt.Print(IMT)
 }
+
+// var userHeight float64 = 1.8
+// var userWeight float64 = 100
+// var IMT = float64(userWeight) / (userHeight)
+// var userKg float64
+// userKg = 100
+// var userHeight, userKg float64 = 1.8, 100
+// userHeight, userKg := 1.8, 100
