f35e5bb8e1e680c843eee0b3922f79f569488c5e
egorfdv <egorfdvru@gmail.com>
2025-03-07 17:03:04 +0300

    Добавлена проверка на норму веса

--- main.go
+++ main.go
@@ -10,11 +10,23 @@ func main() {
 	userHeight, userWeight := getUserInput()
 	IMT := calculateIMT(userWeight, userHeight)
 	outputResult(IMT)
+	switch true {
+	case IMT < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case IMT < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case IMT < 25:
+		fmt.Println("У вас нормальный вес")
+	case IMT < 30:
+		fmt.Println("У вас избыточный вес")
+	default:
+		fmt.Println("У вас степень ожирения")
+	}
 }
 
 func outputResult(IMT float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
-	fmt.Print(result)
+	fmt.Println(result)
 }
 
 func calculateIMT(userWeight float64, userHeight float64) float64 {
@@ -32,3 +44,16 @@ func getUserInput() (float64, float64) {
 	fmt.Scan(&userWeight)
 	return userHeight, userWeight
 }
+
+// Trash
+// if IMT < 16 {
+// 	fmt.Println("У вас сильный дефицит массы тела")
+// } else if IMT < 18.5 {
+// 	fmt.Println("У вас дефицит массы тела")
+// } else if IMT < 25 {
+// 	fmt.Println("У вас нормальный вес")
+// } else if IMT < 30 {
+// 	fmt.Println("У вас избыточный вес")
+// } else {
+// 	fmt.Println("У вас степень ожирения")
+// }
