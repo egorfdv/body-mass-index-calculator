1b981958f005049de1212e0d403cd1e71fbc2398
egorfdv <egorfdvru@gmail.com>
2025-03-07 08:22:30 +0300

    Удаление не нужных частей кода

--- main.go
+++ main.go
@@ -32,22 +32,3 @@ func getUserInput() (float64, float64) {
 	fmt.Scan(&userWeight)
 	return userHeight, userWeight
 }
-
-// var userHeight float64 = 1.8
-// var userWeight float64 = 100
-// var IMT = float64(userWeight) / (userHeight)
-// var userKg float64
-// userKg = 100
-// var userHeight, userKg float64 = 1.8, 100
-// userHeight, userKg := 1.8, 100
-// fmt.Print("__ Калькулятор индекса массы тела __ \n")
-// fmt.Print("Ваш индекс", IMT)
-// fmt.Printf("Ваш индекс массы тела: %v", IMT)
-// fmt.Print(`__ Калькулятор индекса массы тела __
-// Введите свой рост в сантиметрах: %.1f`, 0,1)
-// func outputResult(IMT float64, text string) {
-
-// }
-// func outputResult(IMT, text string) {
-
-// }
