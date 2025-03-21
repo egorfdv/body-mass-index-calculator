a97d38e559ec37b2544afae8a658cf2da368167d
egorfdv <egorfdvru@gmail.com>
2025-03-21 19:03:11 +0300

    Basic functions have been added to the project

--- main.go
+++ main.go
@@ -8,13 +8,28 @@ import (
 const BMIPower = 2
 
 func main() {
+	fmt.Println("__ Body mass index calculator __")
+	var userWeight, userHeight float64 = getUserInput()
+	BMI := calculateBMI(userWeight, userHeight)
+	outputResult(BMI)
+}
+
+func getUserInput() (float64, float64) {
 	var userHeight float64
 	var userWeight float64
-	fmt.Println("__ Body mass index calculator __")
 	fmt.Print("Enter your height (centimeters): ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Enter your weight (kilograms): ")
 	fmt.Scan(&userWeight)
+	return userWeight, userHeight
+}
+
+func calculateBMI(userWeight float64, userHeight float64) float64 {
 	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
-	fmt.Printf("Your body mass index: %.0f", BMI)
+	return BMI
+}
+
+func outputResult(BMI float64) {
+	result := fmt.Sprintf("Your body mass index: %.0f", BMI)
+	fmt.Print(result)
 }
