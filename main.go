9ea0b34d08833ec54befae55c7d6e73d9278b888
egorfdv <egorfdvru@gmail.com>
2025-03-21 15:27:14 +0300

    Added string formatting

--- main.go
+++ main.go
@@ -10,12 +10,11 @@ const BMIPower = 2
 func main() {
 	var userHeight float64
 	var userWeight float64
-	fmt.Print("__ Body mass index calculator __\n")
+	fmt.Println("__ Body mass index calculator __")
 	fmt.Print("Enter your height (centimeters): ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Enter your weight (kilograms): ")
 	fmt.Scan(&userWeight)
 	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
-	fmt.Print("Your body mass index: ")
-	fmt.Print(BMI)
+	fmt.Printf("Your body mass index: %.0f", BMI)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Console application for calculating body mass index
 
-backend v0.0.1
+backend v0.0.1 (20.03.2025)
+backend v0.0.2 (21.03.2025)
