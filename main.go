3fd75ad54d9ab259dad332676293fc5dc06f31ae
egorfdv <egorfdvru@gmail.com>
2025-03-21 15:18:11 +0300

    Added functionality for entering user data

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 const BMIPower = 2
 
 func main() {
-	userHeight := 1.8
-	userWeight := 100.0
-	BMI := userWeight / math.Pow(userHeight, BMIPower)
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("__ Body mass index calculator __\n")
+	fmt.Print("Enter your height (centimeters): ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Enter your weight (kilograms): ")
+	fmt.Scan(&userWeight)
+	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
+	fmt.Print("Your body mass index: ")
 	fmt.Print(BMI)
 }
