72d27ec4ed6aef144f1f371c0ecbd3df3045368e
egorfdv <egorfdvru@gmail.com>
2025-03-22 13:48:15 +0300

    Adding menu functionality and errors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -9,19 +10,17 @@ const BMIPower = 2
 
 func main() {
 	fmt.Println("__ Body mass index calculator __")
-	var userWeight, userHeight float64 = getUserInput()
-	BMI := calculateBMI(userWeight, userHeight)
-	outputResult(BMI)
-	if BMI < 16 {
-		fmt.Println("You are severely underweight")
-	} else if BMI < 18.5 {
-		fmt.Println("You are underweight")
-	} else if BMI < 25 {
-		fmt.Println("You are of normal weight")
-	} else if BMI < 30 {
-		fmt.Println("You are overweight")
-	} else {
-		fmt.Println("You have a degree of obesity")
+	for {
+		var userWeight, userHeight float64 = getUserInput()
+		BMI, err := calculateBMI(userWeight, userHeight)
+		if err != nil {
+			fmt.Println("No parameters specified for calculation")
+		}
+		outputResult(BMI)
+		isRepeatCalculation := checkRepeatCalculation()
+		if !isRepeatCalculation {
+			break
+		}
 	}
 }
 
@@ -35,12 +34,37 @@ func getUserInput() (float64, float64) {
 	return userWeight, userHeight
 }
 
-func calculateBMI(userWeight float64, userHeight float64) float64 {
+func calculateBMI(userWeight float64, userHeight float64) (float64, error) {
+	if userWeight <= 0 || userHeight <= 0 {
+		return 0, errors.New("noParameters")
+	}
 	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
-	return BMI
+	return BMI, nil
 }
 
 func outputResult(BMI float64) {
 	result := fmt.Sprintf("Your body mass index: %.0f", BMI)
 	fmt.Println(result)
+	switch true {
+	case BMI < 16:
+		fmt.Println("You are severely underweight")
+	case BMI < 18.5:
+		fmt.Println("You are underweight")
+	case BMI < 25:
+		fmt.Println("You are of normal weight")
+	case BMI < 30:
+		fmt.Println("You are overweight")
+	default:
+		fmt.Println("You have a degree of obesity")
+	}
+}
+
+func checkRepeatCalculation() bool {
+	var userChoice string
+	fmt.Print("Do you want to repeat the calculation?(y/n): ")
+	fmt.Scan(&userChoice)
+	if userChoice == "Y" || userChoice == "y" {
+		return true
+	}
+	return false
 }
